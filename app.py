ce3fe9628949eb0679f82f6d6396d4ba6d2b7283
Marcišauskas Andrius <andrius.marcisauskas@mif.stud.vu.lt>
2020-02-27 09:34:43 +0200

    Fixed a bug in put

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ from flask_restful import Resource, Api, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-Cars = {0: {'id':0, 'vin':'whd2050041f241488', 'make':'nissan', 'model':'200sx', 'insured':'yes'},
-        1: {'id':1, 'vin':'wdd1458068d241987', 'make':'mazda', 'model':'mx-5', 'insured':'yes'},
-        2: {'id':2, 'vin':'wdd4580011e241548', 'make':'nissan', 'model':'350z', 'insured':'no'},
-        3: {'id':3, 'vin':'wfd2050069l241488', 'make':'nissan', 'model':'300zx', 'insured':'no'},
-        4: {'id':4, 'vin':'wed4870077a581488', 'make':'lexus', 'model':'sc400', 'insured':'no'},
-        5: {'id':5, 'vin':'wgd1080041l666488', 'make':'toyota', 'model':'celica', 'insured':'yes'}
+Cars = {0: {'id':0, 'vin':'whd2050041f241488', 'make':'nissan', 'model':'200sx', 'insurance':'yes'},
+        1: {'id':1, 'vin':'wdd1458068d241987', 'make':'mazda', 'model':'mx-5', 'insurance':'yes'},
+        2: {'id':2, 'vin':'wdd4580011e241548', 'make':'nissan', 'model':'350z', 'insurance':'no'},
+        3: {'id':3, 'vin':'wfd2050069l241488', 'make':'nissan', 'model':'300zx', 'insurance':'no'},
+        4: {'id':4, 'vin':'wed4870077a581488', 'make':'lexus', 'model':'sc400', 'insurance':'no'},
+        5: {'id':5, 'vin':'wgd1080041l666488', 'make':'toyota', 'model':'celica', 'insurance':'yes'}
         };
 keyCounter = 5
 vinArray = ['whd2050041f241488', 'wdd1458068d241987', 'wdd4580011e241548', 
@@ -56,7 +56,7 @@ class Car(Resource):
             vinArray.append(newCar['vin'])
             return "Succesfully posted car with id: " + str(keyCounter), 201
 
-    def put(self, id):
+    def put(self):
         return "Update is not allowed on this page", 400
 
     def delete(self):
@@ -65,6 +65,9 @@ class Car(Resource):
 class CarById(Resource):
     def __init__(self):
         self.parser = reqparse.RequestParser()
+        self.parser.add_argument('vin', type=str, required=True, location='json', help="Vin number must be provided")
+        self.parser.add_argument('make', type=str, required=True, location='json', help="Make must be provided")
+        self.parser.add_argument('model', type=str, required=True, location='json', help="Model must be provided")
         self.parser.add_argument('insurance', type=str, required=True, location='json', help="Insurance status must be provided")
 
     def get(self, id):
@@ -81,15 +84,21 @@ class CarById(Resource):
             return "Wrong car id provided", 404
         else:
             args = self.parser.parse_args()
+            tempVin = Cars[id].get('vin')
+            vinArray.remove(tempVin)
             updateCar = {
             'id':id,
-            'vin':Cars[id].get('vin'),
-            'make':Cars[id].get('make'),
-            'model':Cars[id].get('model'),
+            'vin':args['vin'],
+            'make':args['make'],
+            'model':args['model'],
             'insurance':args['insurance']
             }
-            Cars[id] = updateCar
-            return "Succesfully updated car with id: " + str(id), 200
+            if args['vin'] in vinArray:
+                return "vin number must be unique to the car", 400
+            else:
+                Cars[id] = updateCar
+                vinArray.append(updateCar['vin'])
+                return "Succesfully updated car with id: " + str(id), 200
 
     def delete(self, id):
         if id not in Cars:
