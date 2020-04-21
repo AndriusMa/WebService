b35bfcc45c03f23e7b56d6b409d40ef11fb73108
Marcišauskas Andrius <andrius.marcisauskas@mif.stud.vu.lt>
2020-04-21 18:12:39 +0300

    minor run.sh adjustments

--- stop.sh
+++ stop.sh
@@ -5,7 +5,7 @@ docker rm andriusma_service -f
 docker rm danno_service -f
 echo "Removing local network"
 docker network rm ws_bridge
-docker network rm 2_ws_bridge
+docker network rm webservice_ws_bridge
 echo "Clearing empty images"
 docker rmi $(docker images --filter dangling=true -q --no-trunc)
 echo "Done"
