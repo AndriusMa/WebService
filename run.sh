85bfde174c16289de131393beddca1499efb86b1
Marcišauskas Andrius <andrius.marcisauskas@mif.stud.vu.lt>
2020-04-21 19:18:56 +0300

    cleaning repository

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Create custom local network
-docker network create ws_bridge
-# Run Danno19 Cinema WebService
-cd Danno
-docker-compose up --build -d
-# Run AndriusMa Car WebService
-cd ..
-docker-compose up --build -d
